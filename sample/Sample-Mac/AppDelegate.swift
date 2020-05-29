c96ad41d81ba2b622a041e17608beeb8d81d961f
CavanSu <403029552@qq.com>
2020-05-29 11:45:19 +0800

    add mac target

---
+++ sample/Sample-Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sample-Mac
+//
+//  Created by CavanSu on 2020/5/29.
+//  Copyright © 2020 CavanSu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ sample/Sample-Mac/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  Sample-Mac
+//
+//  Created by CavanSu on 2020/5/29.
+//  Copyright © 2020 CavanSu. All rights reserved.
+//
+
+import Cocoa
+import AlamoClient
+
+class ViewController: NSViewController {
+
+    lazy var client = AlamoClient(delegate: self, logTube: self)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        getRequest()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        }
+    }
+
+    func getRequest() {
+        let url = "http://t.weather.sojson.com/api/weather/city/101030100"
+        let event = RequestEvent(name: "Sample-get")
+        let task = RequestTask(event: event,
+                               type: .http(.get, url: url),
+                               timeout: .low)
+        
+        client.request(task: task, success: AGEResponse.json({ (json) in
+            print("weather json: \(json.description)")
+        })) { (error) -> RetryOptions in
+            print("error: \(error.localizedDescription)")
+            return .resign
+        }
+    }
+    
+    func postRequest() {
+        let url = ""
+        let event = RequestEvent(name: "Sample-post")
+        let parameters: [String: Any]? = nil
+        let headers: [String: String]? = nil
+        let task = RequestTask(event: event,
+                               type: .http(.post, url: url),
+                               timeout: .low,
+                               header: headers,
+                               parameters: parameters)
+        
+        client.request(task: task, success: AGEResponse.json({ (json) in
+            print("weather json: \(json.description)")
+        })) { (error) -> RetryOptions in
+            print("error: \(error.localizedDescription)")
+            return .resign
+        }
+    }
+}
+
+extension ViewController: AlamoClientDelegate {
+    func alamo(_ client: AlamoClient, requestSuccess event: AGERequestEvent, startTime: TimeInterval, url: String) {
+        print("request success, event: \(event.description), url: \(url)")
+    }
+    
+    func alamo(_ client: AlamoClient, requestFail error: ACError, event: AGERequestEvent, url: String) {
+        print("request error, event: \(event.description), error: \(error.localizedDescription), url: \(url)")
+    }
+}
+
+extension ViewController: ACLogTube {
+    func log(from: AnyClass, info: String, extral: String?, funcName: String) {
+        print("info: \(info), extra: \(extral ?? "nil"), funcName: \(funcName)")
+    }
+    
+    func log(from: AnyClass, warning: String, extral: String?, funcName: String) {
+        print("warning: \(warning), extra: \(extral ?? "nil"), funcName: \(funcName)")
+    }
+    
+    func log(from: AnyClass, error: Error, extral: String?, funcName: String) {
+        print("error: \(error), extra: \(extral ?? "nil"), funcName: \(funcName)")
+    }
+}
