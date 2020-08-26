bd2fa5db612a247b7a93df4e824cf5648249b35b
Manshiro Ogikubo <manshiro@gmail.com>
2020-08-27 01:28:01 +0900

    Add

---
+++ AlamoClient/AlamoClient/AlamoClient.h
@@ -0,0 +1,19 @@
+//
+//  AlamoClient.h
+//  AlamoClient
+//
+//  Created by manshiro on 2020/08/25.
+//  Copyright © 2020 Manshiro Ogikubo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlamoClient.
+FOUNDATION_EXPORT double AlamoClientVersionNumber;
+
+//! Project version string for AlamoClient.
+FOUNDATION_EXPORT const unsigned char AlamoClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamoClient/PublicHeader.h>
+
+
